710e31be7f9e36cc66a6126bcf0366ba95d0c6cf
nishino <takaya@gmail.com>
2025-09-28 17:37:04 +0900

    write GSA key from env at boot

---
+++ config/initializers/google_service_account.rb
@@ -0,0 +1,19 @@
+# config/initializers/google_service_account.rb
+require "fileutils"
+require "base64"
+
+target_path = ENV["GOOGLE_APPLICATION_CREDENTIALS"].presence ||
+              "/app/config/keys/google_service_account.json"
+
+b64 = ENV["GOOGLE_SERVICE_ACCOUNT_JSON_BASE64"].to_s.strip
+if b64.empty?
+  Rails.logger.warn "[GSA] GOOGLE_SERVICE_ACCOUNT_JSON_BASE64 is empty; skip writing #{target_path}"
+else
+  begin
+    FileUtils.mkdir_p(File.dirname(target_path))
+    File.binwrite(target_path, Base64.decode64(b64))
+    Rails.logger.info "[GSA] Wrote service account key to #{target_path}"
+  rescue => e
+    Rails.logger.error "[GSA] Failed to write key: #{e.class} #{e.message}"
+  end
+end
