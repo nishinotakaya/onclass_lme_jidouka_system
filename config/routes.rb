7ba014b5e7c71e34b2e317123c4a08e570c3ea66
nishino <takaya@gmail.com>
2025-09-28 15:56:50 +0900

    fix/routeの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 require "sidekiq/web"
 require "sidekiq-scheduler/web"
 
+# config/routes.rb
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # ヘルスチェック用
+  get "/" => proc { [200, {"Content-Type" => "text/plain"}, ["ok"]] }
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
-  mount Sidekiq::Web => '/sidekiq'
+  # （Sidekiq Web UI を使うなら）
+  require "sidekiq/web"
+  mount Sidekiq::Web => "/sidekiq"
 end
